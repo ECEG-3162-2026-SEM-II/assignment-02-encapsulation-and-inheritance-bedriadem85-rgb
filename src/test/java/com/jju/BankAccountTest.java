421ddbb158977503a9008fcf3438a196dcab2223
bedriadem85-rgb <bedriadem85@gmail.com>
2026-04-06 00:55:57 -0700

    Update BankAccountTest.java

--- src/test/java/com/jju/BankAccountTest.java
+++ src/test/java/com/jju/BankAccountTest.java
@@ -1,6 +1,6 @@
 package com.jju;
 
-import static org.junit.jupiter.api.Assertions.*;
+import  static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 
